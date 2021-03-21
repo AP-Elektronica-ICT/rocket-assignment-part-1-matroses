dee04473715d07f2e0f49c5dd716d6a20f2274aa
Matthieu <matthieu.larose@student.ap.be>
2021-03-21 13:36:14 +0100

    RocketGame part 1

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    //Ik maakte gebruik van https://docs.unity3d.com/2017.3/Documentation/ScriptReference/Transform-rotation.html voor de rotatie
+
+    float tiltAngle = -90.0f;
+    Vector3 power = new Vector3(0, 20, 0);
+
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+
+        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(transform.rotation * power);
+        }
+    }
+}
