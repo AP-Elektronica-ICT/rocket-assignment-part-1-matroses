8b56092c9d0bbc3cd162152fc733999503cc92a4
Matthieu <matthieu.larose@student.ap.be>
2021-03-30 22:47:34 +0200

    Menu + ButtonHandler

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OpenLevel(int level) //int level
+    {
+        SceneManager.LoadScene(sceneName: "Assets/Scenes/Levels/Level" + level + ".unity");
+    }
+}
