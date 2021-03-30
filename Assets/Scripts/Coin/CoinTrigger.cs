7a6338dc2544dca1b5cc067fa4a9ec13e97353b8
Matthieu <matthieu.larose@student.ap.be>
2021-03-30 21:41:17 +0200

    Coin + ScoreHandler

---
+++ Assets/Scripts/Coin/CoinTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinTrigger : MonoBehaviour
+{
+    public ScoreHandler scoreHandler;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scoreHandler.AddCoin();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    private int coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = 0;
+    }
+
+    public void AddCoin()
+    {
+        coins++;
+        Debug.Log(coins);
+    }
+}
